83caa083538130c21a06b5016314ad99600def4b
Zia <zia_gt@yahoo.com>
2017-07-24 00:55:28 +0200

    Mockup for find helper

--- js/FindHelper.js
+++ js/FindHelper.js
@@ -1,6 +1,17 @@
 // Find Helper Start
 //...Example 1
-
+const brands = ["Apple", "Sony", "Samsung", "Samsung"];
+const result = brands.find(function(brand){
+    return brand === "Samsung";
+})
+console.log(result);
 //...Example 2
+
+const cars = [
+    {brand:"Ford",price:500,available:2,type:"Sport car"},
+    {brand:"Nisan",price:400,available:2,type:"Wagon"},
+    {brand:"Toyata",price:300,available:2,type:"Sport car"},
+    {brand:"Valvo",price:800,available:2,type:"Truk"}
+];
 
 // Find Helper End

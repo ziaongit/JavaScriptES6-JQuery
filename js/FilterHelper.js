03ed66e3e2e3fe6115ef6c129208315741608cc2
Zia <zia_gt@yahoo.com>
2017-07-24 00:32:43 +0200

    Filter helper added

--- js/FilterHelper.js
+++ js/FilterHelper.js
@@ -1,6 +1,44 @@
 // Filter Helper Start
 //...Example 1
-
+const channel = [
+    {name:'HBO',premium:true},
+    {name:'LIFE',premium:false},
+    {name:'Max',premium:true},
+    {name:'Cooking channel',premium:false},
+    {name:'WOBI',premium:false}
+];
+const premium = channel.filter(function(channel){
+    return channel.premium;
+});
+console.log(premium);
 //...Example 2
+const channels = [
+    {name:'HBO',premium:true},
+    {name:'LIFE',premium:false},
+    {name:'Max',premium:true},
+    {name:'Cooking channel',premium:false},
+    {name:'WOBI',premium:false}
+];
+
+const user = {
+    name:'Zia',
+    premium:'true',
+    premiumChannels:function(){
+        const $this = this;
+        return channels.filter(function(channel){
+            return channel.premium && $this.premium;
+        })
+    },
+    channels:function(){
+        return channels.filter(function(channel){
+            return channel.premium === false;
+        })
+    }
+}
+
+console.log(user.premiumChannels());
+console.log(user.channels());
+
+
 
 // Filter Helper End

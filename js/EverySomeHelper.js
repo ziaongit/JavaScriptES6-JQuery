4d060954ac623eec485a0d10236a4103a7e6abfb
Zia <zia_gt@yahoo.com>
2017-08-01 23:46:40 +0200

    Every and some

--- js/EverySomeHelper.js
+++ js/EverySomeHelper.js
@@ -1,6 +1,20 @@
 // Ever and Some Helper Start
 //...Example 1
+const students = [
+    {name:'Ali', grade:4},
+    {name:'Maria', grade:6},
+    {name:'Sara', grade:8},
+]
+
+const every = students.every(function(student){
+    return student.grade > 4;
+})
+console.log(every);
 
 //...Example 2
+const some = students.some(function(student){
+    return student.grade > 6;
+})
 
+console.log(some);
 // Every and Some Helper End
